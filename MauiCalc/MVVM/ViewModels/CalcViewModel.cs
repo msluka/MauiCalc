a5d8a44de08d59a8f0ed1d887873e99044511269
Msluka <msluka.info@gmail.com>
2023-02-01 23:47:11 +0100

    Make changes for better working

--- MauiCalc/MVVM/ViewModels/CalcViewModel.cs
+++ MauiCalc/MVVM/ViewModels/CalcViewModel.cs
@@ -14,9 +14,49 @@ namespace MauiCalc.MVVM.ViewModels
     {
         public string Formula { get; set; }
         public string Result { get; set; } = "0";
+        public string LastOperator { get; set; } = null;
+        public List<string> NumbersList { get; set; } = new List<string>();
+        public string NumbersListStr { get; set; }
+
+        public string[] operators = { " % ", " / ", " * ", " - ", " + " };
+
+        public void CheckLastOperator(string buttonValue) {
+
+            //string[] operators = { " % ", " / ", " * ", " - ", " + " };
+         
+            if (Array.IndexOf(operators, buttonValue) >= 0) 
+            {
+                if (Array.IndexOf(operators, LastOperator) >= 0)
+                {                   
+
+                }
+                else {
+                    Formula += buttonValue;                 
+                    NumbersList.Add(buttonValue);
+                    NumbersListStr = string.Join(",", NumbersList).Replace(",", " ");
+                    LastOperator = NumbersList.LastOrDefault();
+                }
+            }
+
+
+            else {
+
+                Formula += buttonValue;
+                NumbersList.Add(buttonValue);
+                NumbersListStr = string.Join(",", NumbersList).Replace(",", " ");
+                LastOperator = NumbersList.LastOrDefault();
+            }
+           
+
+        }
+
+        //public ICommand OperationCommand =>
+
+        //    new Command((number) => { Formula += number; });
 
         public ICommand OperationCommand =>
-            new Command((number) => { Formula += number; });
+
+            new Command((number) => { CheckLastOperator(number.ToString()); });
 
         public ICommand ResetCommand =>
             new Command(() =>
@@ -24,6 +64,9 @@ namespace MauiCalc.MVVM.ViewModels
 
                 Result = "0";
                 Formula = "";
+                LastOperator = null;
+                NumbersList.Clear();
+                NumbersListStr = "";
 
             });
 
@@ -32,17 +75,26 @@ namespace MauiCalc.MVVM.ViewModels
 
                 if (Formula.Length >0) {
 
+                    
                     char lastCharacter = Formula[Formula.Length - 1];
                     if (char.IsWhiteSpace(lastCharacter)) {
 
                         // remove last 3 characters from the string
                         Formula = Formula.Substring(0, Formula.Length - 3);
+                        //Remove last element in List
+                        NumbersList.RemoveAt(NumbersList.Count - 1);
+                        NumbersListStr = string.Join(",", NumbersList).Replace(",", " ");
+                        LastOperator = NumbersList.LastOrDefault();
 
                     }
                     else {
 
                         // remove last 1 character from the string
                         Formula = Formula.Substring(0, Formula.Length - 1);
+                        //Remove last element in List
+                        NumbersList.RemoveAt(NumbersList.Count - 1);
+                        NumbersListStr = string.Join(",", NumbersList).Replace(",", " ");
+                        LastOperator = NumbersList.LastOrDefault();
                     }
                     
                 
@@ -53,6 +105,10 @@ namespace MauiCalc.MVVM.ViewModels
         public ICommand CalculateCommand => new Command(() => {
 
             if (Formula.Length == 0)
+            {
+                return;
+            }
+            if (Array.IndexOf(operators, LastOperator) >= 0)
             {
                 return;
             }
